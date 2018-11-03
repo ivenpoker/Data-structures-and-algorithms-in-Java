f19544eb995f07826b5f5f2cc3b4ef3ab38096ab
Happi Yvan <nwaforhappiyvan@gmail.com>
2018-11-03 20:57:43 +0100

    Book 1 introduction. Small introductory program. Ignoring IDE output files.

---
+++ src/Book_1/BookMain.java
@@ -0,0 +1,17 @@
+package Book_1;
+
+public class BookMain {
+    public static void main(String[] args) {
+        System.out.println("###################################################");
+        System.out.println("#                                                 #");
+        System.out.println("#      Book 1: Java How to Program, 10th Edition. #");
+        System.out.println("#      Author: Paul Deitel & Harvey Deitel        #");
+        System.out.println("#                                                 #");
+        System.out.println("###################################################");
+        System.out.println();
+        System.out.println("----------------------------------------------------");
+        System.out.println("   This branch contains solution to each chapter    ");
+        System.out.println("   exercises and projects. Hope you love :)         ");
+        System.out.println("----------------------------------------------------");
+    }
+}
