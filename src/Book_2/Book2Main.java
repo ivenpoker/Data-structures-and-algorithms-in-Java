60b87e5b278b2e8460daf3c648fd3ec2ca345449
Happi Yvan <nwaforhappiyvan@gmail.com>
2018-11-05 09:32:46 +0100

    Initial setup for book-2 branch. Sample introductory program.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/Book_2/Book2Main.java
@@ -0,0 +1,13 @@
+package Book_2;
+
+public class Book2Main {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("|       Book 2: Data Structures and Algorithms in Java 6th Edition |");
+        System.out.println("|       Author: Michael T. Goodrich                                |");
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println();
+        System.out.println("This branch contains the solutions to the Exercises, Projects and \n" +
+                "Creativity problems");
+    }
+}
